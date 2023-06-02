8f6a151363da01d66ee58a58f9c7b4e0898037a9
fredgeorge0311 <george.fred0311@gmail.com>
2023-06-02 12:24:26 -0400

    completed all site --2

--- src/pages/categories/index.tsx
+++ src/pages/categories/index.tsx
@@ -11,7 +11,7 @@ const TermsOfService: NextPageWithLayout = () => {
   return (
     <>
       <DynamicHomeWarning />
-      <DynamicHomePopularCourse />
+      {/* <DynamicHomePopularCourse /> */}
       <DynamicCategoryDetail />
     </>
   )
